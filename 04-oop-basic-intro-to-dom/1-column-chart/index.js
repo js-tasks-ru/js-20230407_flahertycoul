cf0c71c87285e945febd0fb946d9fbf636b9aa6b
boris.molchanov <boris.molchanov@itp-it.com>
2023-04-27 21:27:37 +0400

    04.01 update chart

--- 04-oop-basic-intro-to-dom/1-column-chart/index.js
+++ 04-oop-basic-intro-to-dom/1-column-chart/index.js
@@ -1,33 +1,33 @@
 export default class ColumnChart {
+  chartHeight = 50;
+
   constructor({
-    data,
-    label,
-    value,
-    link,
+    data = [],
+    label = "",
+    value = 0,
+    link = "",
     formatHeading = data => data,
-  }) {
+  } = {}) {
     this.data = data;
     this.label = label;
     this.value = value;
     this.link = link;
     this.formatHeading = formatHeading;
 
-    this.chartHeight = 50;
     this.render();
   }
 
-  getColumnProps() {
+  getChartColumns() {
     const maxValue = Math.max(...this.data);
-    const scale = 50 / maxValue;
+    const scale = this.chartHeight / maxValue;
 
-    return this.data.map(item => {
-      return {
-        percent: (item / maxValue * 100).toFixed(0) + '%',
-        value: String(Math.floor(item * scale))
-      };
-    });
+    return this.data
+      .map(item => {
+        const percent = (item / maxValue * 100).toFixed(0) + '%';
+        return `<div style="--value: ${Math.floor(item * scale)}" data-tooltip="${percent}"></div>`;
+      })
+      .join('');
   }
-
 
   getTemplate() {
     return `
@@ -38,7 +38,7 @@ export default class ColumnChart {
         </div>
         <div class="column-chart__container">
           <div data-element="header" class="column-chart__header">${this.formatHeading(this.value)}</div>
-          <div data-element="body" class="column-chart__chart">${this.getColumnProps()}</div>
+          <div data-element="body" class="column-chart__chart">${this.getChartColumns()}</div>
         </div>
       </div>
     `;
